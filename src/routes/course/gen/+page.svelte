e63ece749f34989a38b84701d97b6faa5eef9096
Arvin Ansari <arvin.ansari68@gmail.com>
2023-11-07 08:27:24 -0800

    feat: added all relevant programming icons from vscode

--- src/routes/course/gen/+page.svelte
+++ src/routes/course/gen/+page.svelte
@@ -1,34 +1,51 @@
 <script lang="ts">
-  import { useChat } from 'ai/svelte';
+	import { useChat } from 'ai/svelte';
+	import { Stepper, Step } from '@skeletonlabs/skeleton';
 
-  const { messages, input, handleSubmit, isLoading } = useChat({
-    api: '/api/course'
-  });
+	const { messages, input, handleSubmit, isLoading } = useChat({
+		api: '/api/course'
+	});
 
-  $: console.log($messages);
+	$: console.log($messages);
 </script>
 
 <main class="flex flex-col items-center py-20">
-  <h1>Search Page</h1>
+	<h1>Search Page</h1>
 
-  <form 
-    on:submit={handleSubmit}
-    class="flex flex-row justify-center p-10" 
-  >
-    <input 
-      name="prompt" 
-      type="text" 
-      placeholder="Search..." 
-      class="shadow-md w-72 px-2"
-      bind:value={$input}
-    >
-    <button type="submit" class="btn variant-filled">Search</button>
-    {#if $isLoading}
-      <div>Loading...</div>
-    {/if}
-  </form>
-  
-  {#each $messages as message}
-    <li>{message.role}: {message.content}</li>
-  {/each}
+	<Stepper on:complete={handleSubmit}>
+		<Step>
+			<svelte:fragment slot="header">Create Your Own Personal Course!</svelte:fragment>
+			<p class="mb-6">
+				With the help of our AI, you can create your own personal course to create any project ideas
+				you have!
+			</p>
+		</Step>
+		<Step>
+			<svelte:fragment slot="header">Select Your Tech-Stack</svelte:fragment>
+      <p class="mb-6">
+        If you know what tech-stack you want to use, you can select it here. Otherwise, you can leave it blank.
+      </p>
+		</Step>
+		<Step>
+			<svelte:fragment slot="header">Describe Your Project</svelte:fragment>
+      <p>Try to describe in detail what your project is about. Make sure to point out key features of your app.</p>
+
+			<div class="flex flex-row justify-center py-4">
+				<textarea
+          class="textarea w-full p-2"
+          rows="4"
+					name="prompt"
+					placeholder="You are presented with an outfit based on your current closet items and based on the weather where the user resides..."
+					bind:value={$input}
+				/>
+				{#if $isLoading}
+					<div>Loading...</div>
+				{/if}
+      </div>
+		</Step>
+	</Stepper>
+
+	{#each $messages as message}
+		<li>{message.role}: {message.content}</li>
+	{/each}
 </main>
