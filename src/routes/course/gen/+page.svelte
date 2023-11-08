4d8eaecb7e02584adfa1f5be3f292b550bf6ee66
Arvin Ansari <arvin.ansari68@gmail.com>
2023-11-08 08:12:51 -0800

    feat: added tech selection

--- src/routes/course/gen/+page.svelte
+++ src/routes/course/gen/+page.svelte
@@ -1,10 +1,39 @@
 <script lang="ts">
 	import { useChat } from 'ai/svelte';
 	import { Stepper, Step } from '@skeletonlabs/skeleton';
+	import Search from 'virtual:icons/tabler/search'
+	
+	import techMappingJson from '$lib/static/tech-mapping.json'
 
 	const { messages, input, handleSubmit, isLoading } = useChat({
 		api: '/api/course'
 	});
+
+	const techMapping: Record<string, string> = techMappingJson;
+	const allTechCount = Object.keys(techMapping).length;
+	const topTechnologies = ['JavaScript', 'NodeJS', 'ReactJS', 'Preact', 'TypeScript', 'Vue', 'Tailwind', 'Mongodb', 'Prisma', 'PostgreSQL', 'MySQL', 'Firebase', 'AWS', 'sass', 'Svelte', 'SvelteKit', 'NextJS', 'ExpressJS'];
+	let searchText = '';
+	let visibleTechs = topTechnologies;
+	let selectedTechs: string[] = ['NodeJS'];
+
+	const handleTechClick = (tech: string) => {
+		if (selectedTechs.includes(tech)) {
+			selectedTechs = selectedTechs.filter((t) => t !== tech);
+			return;
+		}
+		selectedTechs = [...selectedTechs, tech];
+	}
+	const handleSearch = () => {
+		console.log('searchText :>> ', searchText);
+		if (searchText === '') {
+			visibleTechs = topTechnologies;
+			return;
+		} 
+		visibleTechs = Object.keys(techMapping).filter(
+			(tech) => tech.toLowerCase().includes(searchText.toLowerCase())
+		);
+		console.log('visibleTechs :>> ', visibleTechs);
+	}
 
 	$: console.log($messages);
 </script>
@@ -25,12 +54,64 @@
       <p class="mb-6">
         If you know what tech-stack you want to use, you can select it here. Otherwise, you can leave it blank.
       </p>
+
+			<div class="input-group grid-cols-[auto_1fr_auto] w-1/2">
+				<div class="input-group-shim"><Search /></div>
+				<input 
+					class="p-2" 
+					type="search" 
+					placeholder={`Search ${allTechCount} technologies...`} 
+					bind:value={searchText}
+					on:input={handleSearch}
+				/>
+			</div>
+			<div class="flex flex-row items-center flex-wrap gap-2">
+				{#each selectedTechs as tech}
+					<button class="chip variant-soft hover:variant-filled" on:click={() => handleTechClick(tech)}>
+						<img
+							class="w-4 h-auto"
+							src={`/icons/file_type_${techMapping[tech.toLowerCase()]}.svg`}
+							alt={techMapping[tech]}
+						/>
+						<span>{tech}</span>
+					</button>
+				{/each}
+			</div>
+			<div class="card grid grid-cols-6 gap-4 p-4">
+				{#each visibleTechs as tech}
+					<button
+						class="flex flex-col items-center justify-center rounded-md p-2 gap-2"
+						class:variant-filled-tertiary={selectedTechs.includes(tech)}
+						class:shadow-sm={selectedTechs.includes(tech)}
+						on:click={() => handleTechClick(tech)}
+					>
+						<img
+							class="w-8 h-auto"
+							src={`/icons/file_type_${techMapping[tech.toLowerCase()]}.svg`}
+							alt={techMapping[tech]}
+						/>
+						<p class="w-24 text-ellipsis overflow-hidden">{tech}</p>
+					</button>
+				{/each}
+			</div>
 		</Step>
 		<Step>
 			<svelte:fragment slot="header">Describe Your Project</svelte:fragment>
       <p>Try to describe in detail what your project is about. Make sure to point out key features of your app.</p>
 
-			<div class="flex flex-row justify-center py-4">
+			<div class="flex flex-row items-center flex-wrap gap-2">
+				{#each selectedTechs as tech}
+					<button class="chip variant-soft hover:variant-filled" on:click={() => handleTechClick(tech)}>
+						<img
+							class="w-4 h-auto"
+							src={`/icons/file_type_${techMapping[tech.toLowerCase()]}.svg`}
+							alt={techMapping[tech]}
+						/>
+						<span>{tech}</span>
+					</button>
+				{/each}
+			</div>
+			<div class="flex flex-row justify-center pb-4">
 				<textarea
           class="textarea w-full p-2"
           rows="4"
